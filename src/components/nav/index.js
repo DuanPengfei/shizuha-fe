660aae2ff790302fac275192c4ceacd458590393
DuanPengfei <2459714173@qq.com>
2018-02-09 14:04:20 +0800

    fix: export PDF with nav

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -2,17 +2,19 @@
  * @Author: fei
  * @Date: 2018-02-07 15:36:10
  * @Last Modified by: fei
- * @Last Modified time: 2018-02-09 11:11:37
+ * @Last Modified time: 2018-02-09 14:03:14
  */
 
 /**
  * third part module
  */
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import {
     BrowserRouter as Router,
     Link,
-    Route
+    Route,
+    withRouter
 } from 'react-router-dom';
 
 import './assets/nav.css';
@@ -39,25 +41,44 @@ class ToDo extends Component {
     }
 }
 
+const mapStateToProps = (state) => {
+    return ({
+        isPrinting: state.markdown.isPrinting
+    });
+}
 class Nav extends Component {
     render() {
-        return (
-            <div>
-                <div className="nav">
-                    <div className="logo"><img src={logo} /></div>
-                    <ul>
-                        <li><Link to="/todo">ToDo</Link></li>
-                        <li><Link to="/markdown">Markdown</Link></li>
-                    </ul>
-                </div>
+        console.log(this.props.isPrinting);
+        if(this.props.isPrinting) {
+            return (
                 <div>
-                    <Route exact path="/" component={ToDo} />
-                    <Route exact path="/todo" component={ToDo} />
-                    <Route exact path="/markdown" component={MD} />
+                    <div>
+                        <Route exact path="/" component={ToDo} />
+                        <Route exact path="/todo" component={ToDo} />
+                        <Route exact path="/markdown" component={MD} />
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        } else {
+            return (
+                <div>
+                    <div className="nav">
+                        <div className="logo"><img src={logo} /></div>
+                        <ul>
+                            <li><Link to="/todo">ToDo</Link></li>
+                            <li><Link to="/markdown">Markdown</Link></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <Route exact path="/" component={ToDo} />
+                        <Route exact path="/todo" component={ToDo} />
+                        <Route exact path="/markdown" component={MD} />
+                    </div>
+                </div>
+            );
+        }
     }
 }
+Nav = withRouter(connect(mapStateToProps)(Nav));
 
 export default Nav;
